33ad1938aac0962e5a77f7fd11f9596db2f8cbde
Oyewunmi Gbenga <kelvinoye@gmail.com>
2024-01-02 17:26:25 +0100

    Add

--- src/screens/City.js
+++ src/screens/City.js
@@ -38,10 +38,10 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   cityName: {
-    fontSize:40
+    fontSize: 40,
   },
   countryName: {
-    fontSize:30
+    fontSize: 30,
   },
   cityText: {
     justifyContent: "center",
@@ -49,28 +49,27 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "white",
   },
-  populationWrapper:{
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:30
-
+  populationWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
   },
-  populationText:{
-    fontSize:25,
-    marginLeft:7.5,
-    color:"red",
-    fontWeight:"bold"
+  populationText: {
+    fontSize: 25,
+    marginLeft: 7.5,
+    color: "red",
+    fontWeight: "bold",
   },
-  riseSetWrapper:{
-    flexDirection:"row",
-    alignItems:"center",
-    justifyContent:"space-around",
-    marginTop:30
+  riseSetWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginTop: 30,
   },
-  riseSetText:{
-    fontSize:20,
-    color:"#fff",
-    fontWeight:"bold"
-  }
+  riseSetText: {
+    fontSize: 20,
+    color: "#fff",
+    fontWeight: "bold",
+  },
 });
