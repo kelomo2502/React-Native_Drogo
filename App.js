514c320eadedcd453ccea8d4204932d836231831
Oyewunmi Gbenga <kelvinoye@gmail.com>
2024-01-03 16:51:02 +0100

    Add components to navigation

--- App.js
+++ App.js
@@ -4,14 +4,19 @@ import CurrentWeather from "./src/screens/Currentweather";
 import UpComingWeather from "./src/screens/UpComingWeather";
 import City from "./src/screens/City";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <CurrentWeather />
-      </View>
+      <Tab.Navigator>
+       
+          <Tab.Screen name={"Current"} component={CurrentWeather} />
+          <Tab.Screen name={"Upcoming"} component={UpComingWeather} />
+          <Tab.Screen name={"City"} component={City}/>
+        
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
