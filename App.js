3fd0311dba949b2474ed785932e0fce7bf7cdb8e
Oyewunmi Gbenga <kelvinoye@gmail.com>
2024-01-03 17:21:33 +0100

    Add buttom navigation

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
 import CurrentWeather from "./src/screens/Currentweather";
 import UpComingWeather from "./src/screens/UpComingWeather";
 import City from "./src/screens/City";
@@ -11,18 +10,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-       
-          <Tab.Screen name={"Current"} component={CurrentWeather} />
-          <Tab.Screen name={"Upcoming"} component={UpComingWeather} />
-          <Tab.Screen name={"City"} component={City}/>
-        
+        <Tab.Screen name={"Current"} component={CurrentWeather} />
+        <Tab.Screen name={"Upcoming"} component={UpComingWeather} />
+        <Tab.Screen name={"City"} component={City} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
